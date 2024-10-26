6ed22b651b4c13efc77ac70146ad0caa221d5ec8
Joao Vitor <joaovitormoraveis@gmail.com>
2024-10-25 20:20:04 -0400

    configures encryption of properties

--- src/main/java/course/udemy/springboot/loans/dto/LoansContactInfoDto.java
+++ src/main/java/course/udemy/springboot/loans/dto/LoansContactInfoDto.java
@@ -1,11 +1,17 @@
 package course.udemy.springboot.loans.dto;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
 
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
